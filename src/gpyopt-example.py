c0e6a1a32639bbf10dd2ebd594c0cf21ddf500da
Thorben Jensen <jensen.thorben@googlemail.com>
2018-01-20 23:13:21 +0100

    adding parallelization to example script

--- src/gpyopt-example.py
+++ src/gpyopt-example.py
@@ -2,12 +2,43 @@
 
 from GPyOpt.methods import BayesianOptimization
 import numpy as np
+import math
 
-# --- Define your problem
-def f(x): return (6*x-2)**2*np.sin(12*x-4)
-domain = [{'name': 'var_1', 'type': 'continuous', 'domain': (0,1)}]
+# define your problem
+def f(x):
+    return (math.pow(x[0, 0], 2) + x[0, 1])
 
-# --- Solve your problem
-myBopt = BayesianOptimization(f=f, domain=domain)
-myBopt.run_optimization(max_iter=15)
-myBopt.plot_acquisition()
+domain = [{'name': 'x', 'type': 'continuous', 'domain': (-1,1)},
+          {'name': 'y', 'type': 'continuous', 'domain': (0,1)}]
+
+# solve problem
+opt = BayesianOptimization(f=f, domain=domain)
+opt.run_optimization(max_iter=10)
+opt.plot_acquisition()
+opt.plot_convergence()
+
+# show solution
+opt_y = np.min(opt.Y)
+opt_x = opt.X[np.argmin(opt.Y)]
+f(opt_x)
+
+# parallel computation
+opt2 = \
+    BayesianOptimization(f=f,  
+                         domain = domain,                  
+                         acquisition_type = 'EI',              
+                         normalize_Y = True,
+                         initial_design_numdata = 4,
+                         evaluator_type = 'local_penalization',
+                         batch_size = 4,
+                         num_cores = 4,
+                         acquisition_jitter = 0,
+                         verbosity = True)
+
+opt2.run_optimization(max_iter=4)
+opt2.plot_acquisition()
+
+opt2.
+
+
+
